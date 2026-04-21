0252ee9015b1f1071fd9a3ee0e1e1fd73533fe53
elisakappaun-internauta <elisakappaun@gmail.com>
2026-04-21 00:22:14 -0300

    refector: move pasta src para diretorio correto

--- TrabalhoFinalHospital/src/br/com/serratec/conexao/ConnectionFactory.java
+++ TrabalhoFinalHospital/src/br/com/serratec/conexao/ConnectionFactory.java
@@ -5,27 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private String url = "jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:5432/postgres";
+    private String url = "jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:5432/postgres?sslmode=require";
     private String usuario = "postgres.mpmshpcbkhsbvbkysyqi";
     private String senha = "wuLkMFsuaI92st0K";
 
     private  Connection connection;
 
     public Connection getConnection(){
-        System.out.println("--- ~~ Conectando no Banco de Dados ... ");
         try {
+            System.out.println("--- ~~ Conectando no Banco de Dados ~~ ---");
             connection = DriverManager.getConnection(url, usuario, senha);
-            if (connection != null) {
-                System.out.println("Conetado com Sucesso!!!");
-                return connection;   
-            } else {
-                System.out.println("Não foi Possível Conectar!!!");  
-            }
-
         } catch (SQLException e) {
             System.err.println("Problemas no Driver ou Senha Incorreta do Banco!");
             e.printStackTrace();
         }
+        System.out.println("--- ~~ Conectado com sucesso!~~ ---");
         return connection;
     }
 
--- src/src/br/com/serratec/conexao/TesteConexao.java
+++
@@ -1,9 +0,0 @@
-package br.com.serratec.conexao;
-
-import java.sql.Connection;
-
-public class TesteConexao {
-    public static void main(String[] args) {
-        Connection connection = new ConnectionFactory().getConnection();
-    }
-}
