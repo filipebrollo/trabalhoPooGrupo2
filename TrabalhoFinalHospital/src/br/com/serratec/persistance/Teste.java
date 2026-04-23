59af14b5da8720c1734596dc0da8f9f08aae2493
elisakappaun-internauta <elisakappaun@gmail.com>
2026-04-23 17:10:39 -0300

    build: implementação notaFiscalDao

--- TrabalhoFinalHospital/src/br/com/serratec/persistance/Teste.java
+++ TrabalhoFinalHospital/src/br/com/serratec/persistance/Teste.java
@@ -2,6 +2,7 @@ package br.com.serratec.persistance;
 import java.util.Scanner;
 
 import br.com.serratec.Model.Fatura;
+import br.com.serratec.Model.NotaFiscal;
 
 public class Teste {
     public static void main(String[] args) {
@@ -15,7 +16,22 @@ public class Teste {
         Fatura fatura = faturaDao.buscarPorId(id);
 
         System.out.println(fatura);
-        
+
+        if (fatura != null) {
+            NotaFiscal nota = new NotaFiscal(fatura);
+
+            NotaFiscalDao notaFiscalDao = new NotaFiscalDao();
+
+            notaFiscalDao.salvar(nota);
+
+            System.out.println("Nota Fiscal Gerada com Sucesso!!! ");
+            
+        }
+        else{
+            System.out.println("Fatura inexistente!!");
+        }
     }
+
+    
 
 }
